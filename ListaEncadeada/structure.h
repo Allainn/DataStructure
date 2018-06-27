5b85385b55d2092e4492bcf44e06f7b34fa98300
Allainn <allainn@alunos.utfpr.edu.br>
2018-06-27 18:25:59 -0300

    Add inserts Begin/Order/End

--- ListaEncadeada/structure.h
+++ ListaEncadeada/structure.h
@@ -7,10 +7,14 @@ struct no{
 typedef struct list List;
 struct list{
     No *inicio;
+    No *fim;
 };
 
+List* initList();
 No* mallocNo(int id);
-void insertList(List *ptr, No *n);
+void insertEndList(List *ptr, No *n);
+void insertBeginList(List *ptr, No *n);
+void insertOrderList(List *ptr, No *n);
 void removeList(List *ptr, int id);
-void printList(List *ptr);
+void printList(No *ptr);
 void searchList(List* ptr, int id);
